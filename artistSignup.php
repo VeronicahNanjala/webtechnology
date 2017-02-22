3a19d9069434e8a38957ec477d8252daa6d21ee4
veronicah.nanjala <veronicananjala1@gmail.com>
2017-02-22 21:11:00 +0000

    edited form entries with bootsrap

--- artistSignup.php
+++ artistSignup.php
@@ -173,4 +173,5 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
+  <p>This is nonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</p>
 </html>
