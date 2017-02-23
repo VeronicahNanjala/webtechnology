1b57b788cb174439b1d0b5a112fe4460b87067a6
veronicah.nanjala <veronicananjala1@gmail.com>
2017-02-23 01:34:35 +0000

    starting validation

--- artistSignup.php
+++ artistSignup.php
@@ -13,17 +13,58 @@
     <link rel="stylesheet" type="text/css" href="styleSignup.css">
 
   </head>
-  <body style="background-color: black">
-  <!--The navigation bar-->
-
-  <!-- the beginning of php code-->
+  <body style="background-color: skyblue">
+    <!-- the beginning of php code-->
 <?php
 // validating data entered by users 
+// varibles to indicate required fields 
+$nameErr="";
+$emailErr="";
+$userNameErr="";
+$passwordErr="";
+$copyrightErr="";
+$regionErr="";
+$musicTypeErr="";
+$websiteErr="";
+$aboutErr="";
+$contactErr="";
+$birthdateErr="";
 
+// checking if the post method has been requested.
+// checking if the field data are not empty
+if(isset($_POST['submit'])){
+  if (isset($_POST['name']) && !empty($_POST['name'])){
+    $name=$_POST['name'];
+  }
+     else{
+      $nameErr="Name is required";
+      }
 
+  if (isset($_POST['email']) && !empty($_POST['email'])){
+    $email=$_POST['email'];
+    }
+      else{
+      $emailErr="password is required";
+      }
+}
 ?>
 <!-- the end of php code-->
 
+
+  <!--The navigation bar-->
+  <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">NGOMA MUSIC</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Menu 1</a></li>
+      <li><a href="#">Menu 2</a></li>
+      <li><a href="#">Menu 3</a></li>
+    </ul>
+  </div>
+</nav>
 
 <!--Header-->
 <div class="page-header">
@@ -31,50 +72,51 @@
 </div>
 
 <!-- Form for data entry-->
-<form class="form-horizontal" style="background-color: white;width: 50%;margin: auto;" >
-  <div align="center" style="background-color: lightblue">
-  <h1> YOUR HOME FOR MUSIC</h1>
-  <h1>Fill in all the spaces to create an account</h1>
+<form method="post" action="" class="form-horizontal" style="background-color: white;width: 50%;margin: auto;" >
+  <div align="center">
+  <h1> SIGN UP NOW</h1>
   </div>
   <div>
 
   <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Name:</label>
+   <label class="control-label col-sm-2" for="name">Name:</label>
       <div class="col-sm-10">
-        <input type="text" name="name" > <br><br>
+        <input type="text" name="name" >
+          <span style="color: red">*<?php echo $nameErr?></span><br><br>
       </div>
   </div>
 
   <div class="form-group">
    <label class="control-label col-sm-2" for="email">Email:</label>
       <div class="col-sm-10">
-        <input type="text" name="email"> <br><br>
+        <input type="text" name="email"> 
+          <span style="color: red">*<?php echo $emailErr?></span><br><br>
       </div>
   </div>
      
   <div class="form-group">
-   <label class="control-label col-sm-2" for="email">User name:</label>
+   <label class="control-label col-sm-2" for="username">User name:</label>
       <div class="col-sm-10">
         <input type="text" name="username"> <br><br>
       </div>
   </div>
      
   <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Password:</label>
+   <label class="control-label col-sm-2" for="password">Password:</label>
       <div class="col-sm-10">
         <input type="password" name="password"><br><br>
       </div>
   </div>
      
   <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Repeat Password:</label>
+   <label class="control-label col-sm-2" for="password">Repeat Password:</label>
       <div class="col-sm-10">
         <input type="password" name="password"><br><br>
       </div>
   </div>
     
   <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Copyright Registration Number:</label>
+   <label class="control-label col-sm-2" for="copyrightno">Copyright Registration Number:</label>
       <div class="col-sm-10">
         <input type="text" name="copyrightno"> <br><br>
       </div>
@@ -82,21 +124,21 @@
     
   
  <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Region of origin:</label>
+   <label class="control-label col-sm-2" for="region">Region of origin:</label>
       <div class="col-sm-10">
         <input type="text" name="region"> <br><br>
       </div>
   </div> 
     
  <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Type of music:</label>
+   <label class="control-label col-sm-2" for="musictype">Type of music:</label>
       <div class="col-sm-10">
         <input type="text" name="musictype"> <br><br>
       </div>
   </div>  
   
  <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Date of birth:</label>
+   <label class="control-label col-sm-2" for="birthdate">Date of birth:</label>
       <div class="col-sm-10">
         <input type="text" name="birthdate"> <br><br>
       </div>
@@ -104,34 +146,34 @@
    
 
   <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Website:</label>
+   <label class="control-label col-sm-2" for="website">Website:</label>
       <div class="col-sm-10">
         <input type="text" name="website"> <br><br>
       </div>
   </div>
    
   <div class="form-group">
-   <label class="control-label col-sm-2" for="email">About you:</label>
+   <label class="control-label col-sm-2" for="about">About you:</label>
       <div class="col-sm-10">
         <textarea name="about"  rows="4" cols="50"></textarea><br><br>
       </div>
   </div>
   
  <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Contact information:</label>
+   <label class="control-label col-sm-2" for="contact">Contact information:</label>
       <div class="col-sm-10">
         <textarea name="contact"  rows="4" cols="50"></textarea><br><br>
       </div>
   </div>
    
  <div class="form-group">
-   <label class="control-label col-sm-2" for="email">Any other thing for fans:</label>
+   <label class="control-label col-sm-2" for="aob">Any other thing for fans:</label>
       <div class="col-sm-10">
         <textarea name="aob"  rows="4" cols="50"></textarea><br><br>
       </div>
   </div>
 
-   <div><div class="btn btn-default" type="button"><a href="artist.php">Sign up</a>
+   <div><input type="submit" name="submit" class="btn btn-default">
    </div><br><br>
     Already have an account?<div class="btn btn-link"><a href="home.php">Sign in</a></div>
 
@@ -140,38 +182,12 @@
 
 <!--Footer-->
 <footer>
-<div class="col-md-4">
-       <center>
-         <img src="drum.jpg" class="img-circle" alt="...">
-        
-             <h4 class="footertext">Ngoma</h4>
-              <p class="footertext">You african heritage music.<br>
-       </center>
-   </div>
+</footer>
 
-   <div class="col-md-4">
-       <center>
-         <img src="music.jpg" class="img-circle" alt="..." style="float:left;padding-right:10px width: 300px; height: 300px; ">
-     
-             <h4 class="footertext">Ngoma</h4>
-              <p class="footertext">You african heritage.<br>
-       </center>
-   </div>
-
-      <div class="col-md-4">
-       <center>
-         <img src="lingala.jpg" class="img-circle" alt="..." style="float:left;padding-right:10px width: 300px; height: 300px; ">
-  
-             <!-- <h4 class="footertext">Ngoma</h4> 
-              <p class="footertext">You african heritage.<br> -->
-       </center>
-   </div>
-   </footer>
 
     <!--javaScrypt link for bootstrap -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
-  <p>This is nonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</p>
 </html>
