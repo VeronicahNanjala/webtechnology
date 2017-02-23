d73838628e98eb71e7818860f35a4d617c4638b8
veronicah.nanjala <veronicananjala1@gmail.com>
2017-02-23 15:46:43 +0000

    password encryption, password strength and email validation

--- artistSignup.php
+++ artistSignup.php
@@ -30,6 +30,25 @@ $aboutErr="";
 $contactErr="";
 $birthdateErr="";
 
+// variables to store values
+$name="";
+$email="";
+$username="";
+$password="";
+$copyrightno="";
+$region="";
+$musictype="";
+$birthdate="";
+$website="";
+$about="";
+$contact="";
+$aob="";
+
+// variable that holds encrypted password
+// these will be stored in the database
+$passwordEncrypt="";
+$validEmail="";
+
 // checking if the post method has been requested.
 // checking if the field data are not empty
 if(isset($_POST['submit'])){
@@ -41,17 +60,23 @@ if(isset($_POST['submit'])){
       }
 
   if (isset($_POST['email']) && !empty($_POST['email'])){
-    $email=$_POST['email'];
+         $email=$_POST['email'];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+         $emailErr = "Invalid email format"; 
+         }
+         else{
+         $validEmail=$email;
+         }
     }
       else{
-      $emailErr="password is required";
+      $emailErr="Email is required";
       }
 
   if (isset($_POST['username']) && !empty($_POST['username'])){
      $username=$_POST['username'];
      }
       else{
-      $username="Username is required";
+      $userNameErr="Username is required";
       }
 
   if (isset($_POST['copyrightno']) && !empty($_POST['copyrightno'])){
@@ -88,25 +113,52 @@ if(isset($_POST['submit'])){
       else{
       $aboutErr="Required";
       }
+
+  if (isset($_POST['password']) && !empty($_POST['password'])){
+   // encrypting password and checking strength
+      $password=$_POST['password'];
+         if (strlen($password) < 8) {
+              $passwordErr = "Password too short! At least 8 characters";
+          }
+
+          if (!preg_match("#[0-9]+#", $password)) {
+              $passwordErr = "Password must include at least one number!";
+          }
+
+          if (!preg_match("#[a-zA-Z]+#", $password)) {
+              $passwordErr = "Password must include at least one letter!";
+          } 
+            else{
+               $passwordEncrypt=password_hash($password,PASSWORD_DEFAULT);
+            }    
+
+      }
+      else{
+      $passwordErr="Required";
+      }
+
+  if (isset($_POST['contact']) && !empty($_POST['contact'])){
+      $contact=$_POST['contact'];
+      }
+      else{
+      $contactErr="";
+      }
+  if (isset($_POST['aob']) && !empty($_POST['aob'])){
+      $aob=$_POST['aob'];
+      }
+      else{
+      $aobErr="";
+      }
+
+  if (isset($_POST['website']) && !empty($_POST['website'])){
+      $website=$_POST['website'];
+      }
+      else{
+      $websiteErr="";
+      }      
 }
 ?>
 <!-- the end of php code-->
-
-
-  <!--The navigation bar-->
-  <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">NGOMA MUSIC</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Menu 1</a></li>
-      <li><a href="#">Menu 2</a></li>
-      <li><a href="#">Menu 3</a></li>
-    </ul>
-  </div>
-</nav>
 
 <!--Header-->
 <div class="page-header">
@@ -123,7 +175,7 @@ if(isset($_POST['submit'])){
   <div class="form-group">
    <label class="control-label col-sm-2" for="name">Name:</label>
       <div class="col-sm-10">
-        <input type="text" name="name" >
+        <input type="text" name="name" value="<?php echo htmlspecialchars($name);?>">
           <span style="color: red">*<?php echo $nameErr?></span><br><br>
       </div>
   </div>
@@ -131,7 +183,7 @@ if(isset($_POST['submit'])){
   <div class="form-group">
    <label class="control-label col-sm-2" for="email">Email:</label>
       <div class="col-sm-10">
-        <input type="text" name="email"> 
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email);?>"> 
           <span style="color: red">*<?php echo $emailErr?></span><br><br>
       </div>
   </div>
@@ -139,15 +191,15 @@ if(isset($_POST['submit'])){
   <div class="form-group">
    <label class="control-label col-sm-2" for="username">User name:</label>
       <div class="col-sm-10">
-        <input type="text" name="username"> <br><br>
-          <span style="color: red">*<?php echo $usernameErr?></span><br><br>
+        <input type="text" name="username" value="<?php echo htmlspecialchars($username);?>"> 
+          <span style="color: red">*<?php echo $userNameErr?></span><br><br>
       </div>
   </div>
      
   <div class="form-group">
    <label class="control-label col-sm-2" for="password">Password:</label>
       <div class="col-sm-10">
-        <input type="password" name="password"><br><br>
+        <input type="password" name="password" value="<?php echo htmlspecialchars($password);?>">
           <span style="color: red">*<?php echo $passwordErr?></span><br><br>
       </div>
   </div>
@@ -155,7 +207,7 @@ if(isset($_POST['submit'])){
   <div class="form-group">
    <label class="control-label col-sm-2" for="password">Repeat Password:</label>
       <div class="col-sm-10">
-        <input type="password" name="password"><br><br>
+        <input type="password" name="password" value="<?php echo htmlspecialchars($password);?>">
           <span style="color: red">*<?php echo $passwordErr?></span><br><br>
       </div>
   </div>
@@ -163,7 +215,7 @@ if(isset($_POST['submit'])){
   <div class="form-group">
    <label class="control-label col-sm-2" for="copyrightno">Copyright Registration Number:</label>
       <div class="col-sm-10">
-        <input type="text" name="copyrightno"> <br><br>
+        <input type="text" name="copyrightno" value="<?php echo htmlspecialchars($copyrightno);?>">
           <span style="color: red">*<?php echo $copyrightErr?></span><br><br>
       </div>
   </div> 
@@ -172,7 +224,7 @@ if(isset($_POST['submit'])){
  <div class="form-group">
    <label class="control-label col-sm-2" for="region">Region of origin:</label>
       <div class="col-sm-10">
-        <input type="text" name="region"> <br><br>
+        <input type="text" name="region" value="<?php echo htmlspecialchars($region);?>"> 
          <span style="color: red">*<?php echo $regionErr?></span><br><br>
       </div>
   </div> 
@@ -180,14 +232,16 @@ if(isset($_POST['submit'])){
  <div class="form-group">
    <label class="control-label col-sm-2" for="musictype">Type of music:</label>
       <div class="col-sm-10">
-        <input type="text" name="musictype"> <br><br>
+        <input type="text" name="musictype" value="<?php echo htmlspecialchars($musictype);?>"> 
+          <span style="color: red">*<?php echo $musicTypeErr?></span><br><br>
       </div>
   </div>  
   
  <div class="form-group">
    <label class="control-label col-sm-2" for="birthdate">Date of birth:</label>
       <div class="col-sm-10">
-        <input type="text" name="birthdate"> <br><br>
+        <input type="text" name="birthdate" value="<?php echo htmlspecialchars($birthdate);?>"> 
+          <span style="color: red">*<?php echo $birthdateErr?></span><br><br>
       </div>
   </div>  
    
@@ -195,31 +249,31 @@ if(isset($_POST['submit'])){
   <div class="form-group">
    <label class="control-label col-sm-2" for="website">Website:</label>
       <div class="col-sm-10">
-        <input type="text" name="website"> <br><br>
+        <input type="text" name="website" value="<?php echo htmlspecialchars($website);?>"> <br>
       </div>
   </div>
    
   <div class="form-group">
    <label class="control-label col-sm-2" for="about">About you:</label>
       <div class="col-sm-10">
-        <textarea name="about"  rows="4" cols="50"></textarea><br><br>
+        <textarea name="about" rows="4" cols="50">
+        <?php if(isset($about)){ echo htmlspecialchars($about);}?></textarea>
+          <span style="color: red">*<?php echo $aboutErr?></span><br><br>
       </div>
   </div>
   
  <div class="form-group">
    <label class="control-label col-sm-2" for="contact">Contact information:</label>
       <div class="col-sm-10">
-        <textarea name="contact"  rows="4" cols="50"></textarea><br><br>
-      </div>
+        <textarea name="contact"  rows="4" cols="50">
+        <?php if(isset($contact)){echo htmlspecialchars($contact);}?></textarea><br></div>
   </div>
    
  <div class="form-group">
    <label class="control-label col-sm-2" for="aob">Any other thing for fans:</label>
       <div class="col-sm-10">
-        <textarea name="aob"  rows="4" cols="50"></textarea><br><br>
-      </div>
-  </div>
-
+        <textarea name="aob"  rows="4" cols="50">
+        <?php if (isset($aob)){echo htmlspecialchars($aob);}?></textarea><br><br></div></div>
    <div><input type="submit" name="submit" class="btn btn-default">
    </div><br><br>
     Already have an account?<div class="btn btn-link"><a href="home.php">Sign in</a></div>
