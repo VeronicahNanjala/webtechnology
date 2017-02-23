136a04298eda322782b118c0cf7d97e9a48c0b34
TitoLulu <titolulu6@gmail.com>
2017-02-23 16:32:40 +0000

    initial commit

---
+++ events.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	</head>
+	<body>
+		<!--Grid system to accomodate 3 events-->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4" style="background-color:red;"></div>
+				<div class="col-md-4" style="background-color:blue;"></div>
+				<div class="col-md-4" style="background-color:green"></div>
+			</div>
+		</div>
+	</body>
+</html>
