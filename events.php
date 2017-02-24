d3e5f57200f8340659b431b7717fa966430e6e35
TitoLulu <titolulu6@gmail.com>
2017-02-24 21:11:54 +0000

    redesigned look

--- events.php
+++ events.php
@@ -5,26 +5,26 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
-	<body style="background-color: #363636" >
+	<body style="background-color: #29424f" >
 		<!--Grid system to accomodate 3 events-->
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-4">
 						<!-- inserting an upcoming event images in the first column of the grid-->
 						<a href="audience.jpeg" data-lightbox= "This Month">
-							<img src="audience.jpg" width="450px" height="300px">
+							<img src="audience.jpg" width="440px" height="300px" >
 						</a>
 				</div>
 				<div class="col-md-4">
 						<!-- inserting an upcoming event images in the first column of the grid-->
 						<a href="album_launch.jpeg" data-lightbox= "This Month">
-							<img src="album_launch.jpg" width="450px" height="300px">
+							<img src="album_launch.jpg" width="440px" height="300px">
 						</a>
 				</div>
 				<div class="col-md-4" >
 						<!-- inserting an upcoming event images in the first column of the grid-->
 						<a href="audience.jpeg" data-lightbox= "This Month">
-							<img src="audience.jpg " width="450px" height="300px">
+							<img src="audience.jpg " width="440px" height="300px " style="margin-right:10px">
 						</a>
 				</div>
 			</div>
