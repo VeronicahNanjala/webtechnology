d3c99c6aa38cd33f28f90741c68cb3ae68879a81
Bridgett Marufu <Bridgett Marufu>
2017-02-24 01:56:53 +0000

    modified skeleton of contact form

--- contactUs.php
+++ contactUs.php
@@ -5,11 +5,26 @@
 <body>
 
 <form name="myForm" action="/action_page_post.php" onsubmit="return validateForm()" method="post">
-	<table>
-		Name: <input type="text" name="fname"> <br>
-		Email: <input type="text" name ="femail"><br>
-		Message: <input type="text" name = "fmessage"><br>
-		<input type="submit" value="Submit">
+	<table style = "padding-top: 15%; margin:auto;">
+	<tr>
+	<td> Name</td>
+	<td> <input type="text" name="fname" size="52%"> <br></td>
+	</tr>
+
+	<tr>
+	<td> Email</td>
+	<td><input type="text" name ="femail" size="52%"><br></td>
+	</tr>
+		<tr>
+	<td>Message</td>
+	<td><textarea type="text" name="fmessage" rows="5" cols="40"></textarea><br><br></td>
+	</tr>
+
+	<tr>
+	<td></td>
+	<td style="padding-left: 69.5%"><input type="submit" value="Submit" ></td>
+	</tr>
+
 	</table>
 </form>
 
