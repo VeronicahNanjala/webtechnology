1d8a32a773706a2db34a59e8d36f45114e887195
TitoLulu <titolulu6@gmail.com>
2017-02-25 16:38:21 +0000

    cleaned up bootstrap code and added an external stylesheet

--- events.php
+++ events.php
@@ -4,38 +4,64 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="custom.css">
 	</head>
-	<body style="background-color: #29424f" >
+	<body  >
 		<!--Grid system to accomodate 3 events-->
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-lg-4 ">
 						<!-- inserting an upcoming event images in the first column of the grid-->
 						<a href="audience.jpeg" data-lightbox= "This Month">
-							<img src="audience.jpg" width="440px" height="300px">
+							<img src="audience.jpg" class= "img-rounded" width="440px" height="300px">
 						</a>
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<div class="caption">
+									<h3 >Lagos Photo</h3>
+									<p>Challenging  World's Afro-Pessimism</p>
+								</div>
+							</div>
+						</div>
 				</div>
 				<div class="col-lg-4 ">
 						<!-- inserting an upcoming event images in the first column of the grid-->
 						<a href="album_launch.jpeg" data-lightbox= "This Month">
-							<img src="album_launch.jpg" width="440px" height="300px">
+							<img src="album_launch.jpg" class="img-rounded" width="440px" height="300px">
 						</a>
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<div class="caption">
+									<h3>Blanket & Wine Festival</h3>
+									<p>Explore African Music Through the Eyes of Nairobi</p>
+								</div>
+							</div>
+						</div>
 				</div>
 				<div class="col-lg-4 " >
 						<!-- inserting an upcoming event images in the first column of the grid-->
 						<a href="dancehall.jpeg" data-lightbox= "This Month">
-							<img src="dancehall.jpg " width="440px" height="300px " style="margin-right:10px">
+							<img src="dancehall.jpg " class="img-rounded" width="440px" height="300px ">
 						</a>
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<div class="caption">
+									<h3>Ultra Music Festival Johburg</h3>
+									<p>Live the experience of African House and Trap Music</p>
+								</div>
+							</div>
+						</div>
 				</div>
 			</div>
-			<!-- button linking homepage events section with primary events page -->
-			<div class="row">
-				<div class="col-lg-4"></div>
-				<div class="col-lg-4">
-					<a href="#"><button type="button" class="btn btn-primary btn-block active" aria-label="See More EVents" style="margin-top:10px">
-				</a>Explore More Events
-				</div>
-				<div class="col-lg-4"></div>
+		
+		</div>
+		<!-- Wrapping button, linking events section to main events landing page, inside a jumbotron -->
+		<div class="jumbotron">
+			<div class="container">
+				<a href="#" Id="link">
+					<button type="button" class="btn btn-primary btn-block active">
+				</a>
+					Explore More Events
 			</div>
 		</div>
 	</body>
