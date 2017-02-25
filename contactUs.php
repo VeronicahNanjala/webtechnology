aaf2a2840ac092dd228d1f6f3e880d09b03c2e25
Bridgett Marufu <Bridgett Marufu>
2017-02-25 17:00:04 +0000

    commenting on the contact us page

--- contactUs.php
+++ contactUs.php
@@ -3,7 +3,7 @@
 
 </head>
 <body>
-
+<!--A form for the contact us page-->
 <form name="myForm" action="/action_page_post.php" onsubmit="return validateForm()" method="post">
 	<table style = "padding-top: 15%; margin:auto;">
 	<tr>
@@ -29,17 +29,21 @@
 </form>
 
 <script>
+//A function that validates the contents of the form
 function validateForm() {
+	//check if name is not empty
     var x = document.forms["myForm"]["fname"].value;
     if (x == "") {
         alert("Name must be filled out");
         return false;
     }
+    //check if email is not empty
      var y = document.forms["myForm"]["femail"].value;
     if (y == "") {
         alert("Email must be filled out");
         return false;
         }
+    //check if message box is not empty
          var z = document.forms["myForm"]["fmessage"].value;
     if (z == "") {
         alert("Tell us your message");
