79e09f4ae688827b59e1642c47361cef545d27d9
Bridgett Marufu <Bridgett Marufu>
2017-02-25 16:42:37 +0000

    Modifications on the listener sign up page

--- listenerSignUp.php
+++ listenerSignUp.php
@@ -8,33 +8,33 @@
 	<table style = "padding-top: 15%; margin:auto;">
 	<tr>
 	<td> First name</td>
-	<td> <input type="text" name="fname" size="52%"> <br></td>
+	<td> <input type="text" name="fname" size="52%"> <br><br></td>
 	</tr>
 
 	<tr>
 	<td> Surname</td>
-	<td> <input type="text" name="fname" size="52%"> <br></td>
+	<td> <input type="text" name="fsurname" size="52%"> <br><br></td>
 	</tr>
 
 
 	<tr>
 	<td> Email</td>
-	<td><input type="text" name ="femail" size="52%"><br></td>
+	<td><input type="text" name ="femail" size="52%"><br><br></td>
 	</tr>
 
 	<tr>
-	<td> Username name</td>
-	<td> <input type="text" name="fname" size="52%"> <br></td>
+	<td> Username</td>
+	<td> <input type="text" name="fusername" size="52%"> <br><br></td>
 	</tr>
 
 	<tr>
 	<td>Password</td>
-	<td><input type="password" name="fpassword" size="52%"></textarea><br></td>
+	<td><input type="password" name="fpassword" size="52%"><br><br></td>
 	</tr>
 
 	<tr>
 	<td>Confirm Password</td>
-	<td><input type="password" name="fpassword" size="52%"></textarea><br><br></td>
+	<td><input type="password" name="fcpassword" size="52%"><br><br></td>
 	</tr>
 
 	<tr>
@@ -49,18 +49,37 @@
 function validateForm() {
     var x = document.forms["myForm"]["fname"].value;
     if (x == "") {
-        alert("Name must be filled out");
+        alert("First name must be filled out");
         return false;
     }
-     var y = document.forms["myForm"]["femail"].value;
+     var y = document.forms["myForm"]["fsurname"].value;
     if (y == "") {
         alert("Email must be filled out");
         return false;
         }
-         var z = document.forms["myForm"]["fmessage"].value;
+         var z = document.forms["myForm"]["femail"].value;
     if (z == "") {
         alert("Tell us your message");
         return false;
+    }
+     var a = document.forms["myForm"]["fusername"].value;
+     if (a == "") {
+        alert("Please enter a username");
+        return false;
+    }
+     var b = document.forms["myForm"]["fpassword"].value;
+     if (b == "") {
+        alert("Please nter password");
+        return false;
+    }
+     var c = document.forms["myForm"]["fcpassword"].value;
+     if (c == "") {
+        alert("Confirm password");
+        return false;
+    }
+    if(c!=b){
+    	alert("Password mismatch, please try again");
+    	return false;
     }
 
 }
