dab3358df2a6aa5f6cff3cf95669758c458d2764
lesapiti-loltolo <petrolesapiti@gmail.com>
2017-02-25 16:33:45 +0000

    Updated the css, html and the js file

--- login.js
+++ login.js
@@ -2,7 +2,6 @@
 This class validates the password counts and sets a temporary password
 */
 //A variable that counts the number of times someone tried a password
-passwordAttempt --5;
 function validate(){
 var username = document.getElementById("uname").value;
 var password = document.getElementById("psw").value;
@@ -12,17 +11,35 @@ alert ("Login successfully");
 //window.location = "x.html"; // Redirecting to other page.
 return false;
 }
-else{
+else if(username==""){
 
-passwordAttempt --;// Decrementing by one.
-alert("You have left "+passwordAttempt+" attempt;");
-// Disabling fields after 3 attempts.
-if( passwordAttempt == 0){
-	//alert("Hello");
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
+document.getElementById("uname").style.borderColor="red";
+
 }
+else if(password==""){
+
+document.getElementById("psw").style.borderColor="red";
+
+}
+else if(!/^\w+$/.test( username )) {
+    alert("Username must contain only letters, numbers and underscores!");
+    }
+else if(password.length<6){
+	alert("The password is too short")
+}
+else if(username==password){
+ alert("Username and password should be different")
+}
+else if(!/[0-9]/.test(password)){
+  alert("Password must contain alert one number(0-9)")
+}
+else if(!/[a-z]/.test(password)){
+  alert("Password must contain atleast one lowercase character(a-z)")
+}
+else if(!/[A-Z]/.test(password)){
+  alert("Password must contain atleast one uppercase character(A-Z)")
+}
+else{
+	alert("Wrong Password or Username")
 }
 }
