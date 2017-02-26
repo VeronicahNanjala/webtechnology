1782ea3b611a3267f679e65a0a529d6e3495a15d
TitoLulu <titolulu6@gmail.com>
2017-02-26 23:14:49 +0000

    added footer

--- Events_landingpage.php
+++ Events_landingpage.php
@@ -15,6 +15,14 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="Home.css">
 		<link rel="stylesheet" type="text/css" href="landing.css">
+		<link rel="stylesheet" href="assets/css/fancybox/jquery.fancybox.css">
+		<link href="assets/css/bootstrap.css" rel="stylesheet" />
+		<link href="assets/css/bootstrap-theme.css" rel="stylesheet" />
+		<link rel="stylesheet" href="assets/css/slippry.css">
+		<link href="assets/css/style.css" rel="stylesheet" />
+		<link rel="stylesheet" href="assets/color/default.css">
+		<script src="assets/js/modernizr.custom.js"></script>
+
 		<style>	
 			#lst a:hover{
 				color:green;
@@ -22,31 +30,45 @@
 		</style>
 	</head>
 	<body>
-		<nav class="navbar navbar-default">
-		<div class="container-fluid">
-		    <div class="navbar-header">
-				<a class="navbar-brand"><h1>Ngoma<h1></a>
-			</div>
-			<div>
-			<div class="row">
-			  <div class="col-md-9"></div>
-			  <div class="col-md-3"></div>
-				<ul class="nav navbar-nav navbar-right">
-					<li id="lst"><a href="index.html #intro">HOME</a></li>
-					<li id="lst"><a href="index.html #about">ABOUT</a></li>
-					<li id="lst"><a href="index.html #works">LOGIN</a></li>
-					<li id="lst"><a href="index.html #works">SIGNUP</a></li>
-					<li id="lst"><a href="index.html #events">EVENTS</a></li>
-					<li id="lst"><a href="index.html #contact">CONTACT US</a></li>
-				
-				</ul>
-			</div>
-		</div>
-		</nav>
+		<section>
+			<header>
+				<div id="navigation" class="navbar navbar-inverse navbar-fixed-top default" role="navigation">
+				  <div class="container">
+
+					<!--  Header view - navigation bar
+						  Brand and toggle get grouped for better mobile display -->
+
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+
+					  <a class="navbar-brand" href="index.html"> Ngoma </a>
+
+					</div>
+
+					<div class="navigation">
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><nav>
+
+					  <ul class="nav navbar-nav navbar-right">
+						<li class="current"><a href="index.html #intro"> Home </a></li>
+						<li><a href="index.html #about"> About </a></li>
+						<li><a href="index.html #events">Events</a><li>
+						<li><a href="index.html #contact"> Contact Us </a></li>
+					  </ul></nav>
+					</div> <!-- /.navbar-collapse -->
+					</div>
+				  </div>
+				</div>
+
+			</header>
+		</section>
 		<!--columns to create space in canvas to hold event posters -->
 		
-			<div class="jumbotron">
-				<h4>Events Happening this Month<h4>
+		<div class="jumbotron">
+			<h4>Events Happening this Month<h4>
 			</div> <!--signals a row of events happening in the same time period-->
 				<div> <!--first row-->
 					<div class="row">
@@ -136,6 +158,44 @@
 					</div>
 				</div>
 			</div>
+			
+			<footer>
+				<div class="verybottom">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="aligncenter">
+								<ul class="social-network social-circle">
+									<li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+									<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+								</ul>	
+								</div>				
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="aligncenter">
+									<p>
+										 &copy; Ngoma Music - All right reserved
+									</p>
+									<div class="credits">
+										<!-- 
+											All the links in the footer should remain intact. 
+											You can delete the links only if you purchased the pro version.
+											Licensing information: https://bootstrapmade.com/license/
+											Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Groovin
+										-->
+										<a href="https://bootstrapmade.com/">Credit:Free Bootstrap Themes</a> by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				</footer>
 			
 			
 			
