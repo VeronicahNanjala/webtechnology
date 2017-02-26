4ff348ce60e74b5c3fb12671ac346fdb57365063
Veronicah Nanjala <veronicananjala1@gmail.com>
2017-02-26 03:04:44 +0000

    Changed all validation to JavaScript from php
    just changed to Javas crypt

--- artistSignup.php
+++ artistSignup.php
@@ -235,6 +235,8 @@ return true;
 <footer>
 </footer>
 
+    
+    
 
     <!--javaScrypt link for bootstrap -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
