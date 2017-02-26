4eb42d74e4a74a488aacb9c705ac25e855248b7b
TitoLulu <titolulu6@gmail.com>
2017-02-26 18:45:11 +0000

    corrected a deletion due to process failure

---
+++ Events_landingpage.php
@@ -0,0 +1,145 @@
+<!--
+	Landing page acting as main events page
+	The page displays events according to how close their due dates are in a month
+-->
+<!DOCTYPE html>
+<html>
+	<head> 
+		<title>
+		Number One Choice for African music
+		</title>
+		<!-- Including bootstrap in HTML code
+		-->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="Home.css">
+		<link rel="stylesheet" type="text/css" href="landing.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+		<div class="container-fluid">
+		    <div class="navbar-header">
+				<a class="navbar-brand" href="#"><h1>Ngoma<h1></a>
+			</div>
+			<div>
+			<!--<ul class="nav navbar-nav">
+				<li id="hm"><a>Home</a></li>
+				<li id="slash"><a>|</a></li>
+				
+			</ul>-->
+			<div class="row">
+			  <div class="col-md-9"></div>
+			  <div class="col-md-3"></div>
+			  <button type="button" class="btn btn-default btn-sm" style="margin-top: 10px;" >Sign up</button>
+			  <button type="button" class="btn btn-default btn-sm" style="margin-top: 10px;" >Create account</button>
+			</div>
+		</div>
+		</nav>
+		<!--columns to create space in canvas to hold event posters -->
+		
+			<div class="jumbotron">
+				<h4>Events Happening this Month<h4>
+			</div> <!--signals a row of events happening in the same time period-->
+				<div> <!--first row-->
+					<div class="row">
+						<div class="col-lg-4">
+							<a href="audience.jpg" class="fancybox">
+								<img src="audience.jpg " width="440 px" height="380px">
+							</a>
+						</div>
+						<div class="col-lg-4">
+							<a href="dancehall.jpg" data-lightbox="May" class="fancybox">
+								<img src="dancehall.jpg" width="440 px" height="380px" >
+							</a>
+						</div>
+						<div class="col-lg-4">
+							<a href="album_launch.jpg" data-lightbox="May" class="fancybox">
+								<img src="album_launch.jpg" width="440 px" height="380px" >
+							</a>
+						</div>
+					</div>
+				</div>
+			<div class="jumbotron">
+				<h4>Events Happening in March<h4>
+			</div><!--signals a row of events happening in the same time period-->
+			
+			<div> <!--second row-->
+				<div class="row">
+					<div class="col-lg-4">
+						<a href="audience.jpg" data-lightbox="May" class="fancybox">
+							<img src="audience.jpg" width="440 px" height="380px">
+						</a>
+					</div>
+					<div class="col-lg-4">
+						<a href="dancehall.jpg" data-lightbox="May" class="fancybox">
+							<img src="dancehall.jpg" width="440 px" height="380px" >
+						</a>
+					</div>
+					<div class="col-lg-4">
+						<a href="album_launch.jpg" data-lightbox="May" class="fancybox">
+							<img src="album_launch.jpg" width="440 px" height="380px">
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="jumbotron">
+				<h4>Events Happening in April<h4>
+			</div><!--signals a row of events happening in the same time period-->
+			
+			<div> <!--third row-->
+				<div class="row">
+					<div class="col-lg-4">
+						<a href="audience.jpg" data-lightbox="May" class="fancybox">
+							<img src="audience.jpg" width="440 px" height="380px"">
+						</a>
+					</div>
+					<div class="col-lg-4">
+						<a href="dancehall.jpg" data-lightbox="May" class="fancybox">
+							<img src="dancehall.jpg" width="440 px" height="380px">
+						</a>
+					</div>
+					<div class="col-lg-4">
+						<a href="album_launch.jpg" data-lightbox="May" class="fancybox">
+							<img src="album_launch.jpg" width="440 px" height="380px">
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="jumbotron">
+				<h4>Events Happening in May<h4>
+			</div><!--signals a row of events happening in the same time period-->
+			
+			<div> <!--Fourth  row-->
+				<div class="row">
+					<div class="col-lg-4" data-lightbox="May">
+						<a href="audience.jpg" class="fancybox" >
+							<img src="audience.jpg" width="440 px" height="380px">
+						</a>
+					</div>
+					<div class="col-lg-4" data-lightbox="May">
+						<a href="dancehall.jpg" class="fancybox">
+							<img src="dancehall.jpg" width="440 px" height="380px">
+						</a>
+					</div>
+					<div class="col-lg-4" data-lightbox="May">
+						<a href="album_launch.jpg" class="fancybox" >
+							<img src="album_launch.jpg" width="440 px" height="380px">
+						</a>
+					</div>
+				</div>
+			</div>
+			
+			
+			
+		
+		
+			
+	</body>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<link rel="stylesheet" href="/fancybox/source/jquery.fancybox.css?v=2.1.6" type="text/css" media="screen" />
+	<script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js?v=2.1.6"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
