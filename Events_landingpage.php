e0a7aa85d7da300be24cde4a97a16c6d3b4418d9
TitoLulu <titolulu6@gmail.com>
2017-02-26 13:14:14 +0000

    added jumbotron class

--- Events_landingpage.php
+++ Events_landingpage.php
@@ -19,7 +19,7 @@
 		<nav class="navbar navbar-default">
 		<div class="container-fluid">
 		    <div class="navbar-header">
-				<a class="navbar-brand" href="#">Ngoma</a>
+				<a class="navbar-brand" href="#"><h1>Ngoma<h1></a>
 			</div>
 			<div>
 			<!--<ul class="nav navbar-nav">
@@ -34,68 +34,84 @@
 			  <button type="button" class="btn btn-default btn-sm" style="margin-top: 10px;" >Create account</button>
 			</div>
 		</div>
+		</nav>
 		<!--columns to create space in canvas to hold event posters -->
 		<section>
-			<div> <!--first row-->
-				<div class="row">
-					<div class="col-lg-4">
-						<a>
-						</a>
-					</div>
-					<div class="col-lg-4">
-						<a>
-						</a>
-					</div>
-					<div class="col-lg-4">
-						<a>
-						</a>
+			<div class="jumbotron">
+				<h4>Events Happening this Month<h4>
+			</div> <!--signals a row of events happening in the same time period-->
+				<div> <!--first row-->
+					<div class="row">
+						<div class="col-lg-4">
+							<a href="#">
+							</a>
+						</div>
+						<div class="col-lg-4">
+							<a href="#">
+							</a>
+						</div>
+						<div class="col-lg-4">
+							<a href="#">
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
+			<div class="jumbotron">
+				<h4>Events Happening in March<h4>
+			</div><!--signals a row of events happening in the same time period-->
+			
 			<div> <!--second row-->
 				<div class="row">
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 				</div>
 			</div>
+			<div class="jumbotron">
+				<h4>Events Happening in April<h4>
+			</div><!--signals a row of events happening in the same time period-->
+			
 			<div> <!--third row-->
 				<div class="row">
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 				</div>
 			</div>
+			<div class="jumbotron">
+				<h4>Events Happening in May<h4>
+			</div><!--signals a row of events happening in the same time period-->
+			
 			<div> <!--Fourth  row-->
 				<div class="row">
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 					<div class="col-lg-4">
-						<a>
+						<a href="#">
 						</a>
 					</div>
 				</div>
