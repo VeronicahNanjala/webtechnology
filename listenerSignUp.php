9a8aceaa3b6f96f0fc0cf6f19cf9e6fe2d94e43c
lesapiti-loltolo <petrolesapiti@gmail.com>
2017-02-27 01:05:11 +0000

    added all the files

--- listenerSignUp.php
+++ listenerSignUp.php
@@ -63,11 +63,8 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><nav>
 
       <ul class="nav navbar-nav navbar-right">
-        <li class="current"><a href="#intro"> Home </a></li>
-        <li><a href="#about"> About </a></li>
-        <li><a href="#works"> Sign Up </a></li>
-        <li><a href="Login.html"> Sign In</a></li>
-        <li><a href="#contact"> Contact Us </a></li>
+        <li><a href="home.html"> Home </a></li>
+        <li><a href="contactUs.php"> Contact Us </a></li>
       </ul></nav>
     </div> <!-- /.navbar-collapse -->
     </div>
