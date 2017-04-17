422d97135ba0f02212e2474bf4e2377e4a4b8dbf
veronicah.nanjala <veronicananjala1@gmail.com>
2017-04-17 21:24:40 +0000

    merge conflicts

--- webtechnology/final/database/dbclass.php
+++ webtechnology/final/database/dbclass.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 /*
 *@author Tito Lulu Magero
@@ -69,76 +68,4 @@ class databaseConnection{
 		
 	}
 }
-=======
-<?php 
-/*
-*@author Tito Lulu Magero
-*database class detailing database actions
-*/
-require_once('databaseconnection.php');
-
-class databaseConnection{
-	public $connect;
-	public $result;
-	
-	/*establish connection
-	*using connection method
-	*@return return true or false
-	*/
-	public function connect2db(){
-		$this->connect=new mysqli($hostname, $dname,$uname,$pwd);
-		
-		if(mysqli_connect_errno()){
-			return false;
-			
-		}
-		else{
-			return true;
-		}
-		
-	}
-	/* allow querying using query method
-	*@return return true or false
-	*/
-	public function querydb($sql){
-		if(!$this->connect()){
-			return false;
-		}
-		else{
-			$this->result=mysqli_query($this->connect,$sql);
-		}
-		//check records returned
-		if($this->result ==false){
-			return false;
-			
-		}else{
-			return true;
-		}
-	}
-	/*fetch data in database using fetch method
-	*@return return true or false
-	*/
-	public function fetchdb(){
-		if($this->result==false){
-			return false;
-		}
-		else{
-			mysqli_fetch_assoc($this->result);
-		}
-		
-	}
-	/*prevent sql injection using prevent injection method
-	*@return return true or false
-	*/
-	public function preventsqlInj($sql){
-		
-	}
-	/*prepare statement function
-	*@return return true or false
-	*/
-	public function prepStatement(){
-		
-	}
-}
->>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 ?>
