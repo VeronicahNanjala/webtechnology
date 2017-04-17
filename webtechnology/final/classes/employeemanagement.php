e67f5a1ca3fedc2e141fd66e0cfba6b782f9b9f2
veronicah.nanjala <veronicananjala1@gmail.com>
2017-04-17 21:12:25 +0000

    Resloved conflicts

--- webtechnology/final/classes/employeemanagement.php
+++ webtechnology/final/classes/employeemanagement.php
@@ -6,7 +6,11 @@
 /**
 * @author Veronicah Nanjala
 */
+<<<<<<< HEAD
 class EmplyoyeeManagement
+=======
+class ClassName extends AnotherClass
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 {
 	private $firstname;
 	private $lastname;
@@ -16,6 +20,10 @@ class EmplyoyeeManagement
 	private $contactnumber;
 	private $role;
 
+<<<<<<< HEAD
+
+=======
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 	function __construct()
 	{
 		# code...
@@ -24,6 +32,12 @@ class EmplyoyeeManagement
 /*
 *Set the id of the employee 
 */
+<<<<<<< HEAD
+    /**
+     * @param $id
+     */
+=======
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 public function setId($id){
 $this->employeeid=$id;
 }
@@ -33,7 +47,11 @@ $this->employeeid=$id;
 * @return return employeeId
 */
 public function getId(){
+<<<<<<< HEAD
+return $this->employeeid;
+=======
 return $this->employeeId;
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 }
 
 /*
@@ -96,7 +114,11 @@ return $this->employmentdate;
 *
 */
 public function setRole($rl){
+<<<<<<< HEAD
+$this->role=$rl;
+=======
 $this->role=$rl
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 }
 
 /*
@@ -110,7 +132,11 @@ return $this->role;
 *
 */
 public function setContactNumber($contact){
+<<<<<<< HEAD
+$this->contactnumber=$contact;
+=======
 $this->contactnumber=$contact
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 }
 
 /*
@@ -120,6 +146,13 @@ public function getContactNumber(){
 return $this->contactnumber;
 }
 
+<<<<<<< HEAD
+    /**
+     *Adding employee to the database
+     */
+    public function addEmployee(){
+    require_once("../database/dbclass.php");
+=======
 
 
 /*
@@ -130,20 +163,29 @@ return $this->contactnumber;
 *Adding employee to the database
 */
 public function addEmployee(){
-require_once("../database/dbclass.php");
+// require once db connection
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 	$sql="INSERT INTO Employee empID=?,firts_name=?,last_name=?,employmentDate=?,gender=?,contact_no=?,role=?";
 
 	$stmt = $dbconn->prepare($sql);
 
 	$id=$this->employeeid;
 	$fname=$this->firstname;
+<<<<<<< HEAD
+	$lname=$this->lastname;
+=======
 	$laname=$this->lastname;
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 	$edate=$this->employmentdate;
 	$gen=$this->gender;
 	$contact=$this->contactnumber;
 	$rl=$this->role;
 
+<<<<<<< HEAD
+	$stmt->bind_param('i','s','s','s','s','i','s', $id,$fname,$lname,$edate,$gen,$contact,$rl);
+=======
 	$stmt->bind_param('i','s','s','s','s','i','s', $id,$fname,$lname,$edate,$gender,$contact,$rl);
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 	$stmt->execute(); 
     $stmt->close(); 
 }
@@ -152,7 +194,11 @@ require_once("../database/dbclass.php");
 *Deleting empployee from the database
 */
 public function deleteEmployee(){
+<<<<<<< HEAD
 require_once("../database/dbclass.php");
+=======
+// require once db connection
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 	$id=$this->employeeid;
 	$sql="DELETE FROM Employee WHERE WHERE empID='$id'";
 
@@ -167,7 +213,11 @@ require_once("../database/dbclass.php");
 *Updating an emplyee in the database
 */
 public function updateEmployee(){
+<<<<<<< HEAD
 require_once("../database/dbclass.php");
+=======
+// require once db connection
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 
 	$id=$this->employeeid;
 	$sql="UPDATE Employee SET firts_name=?,last_name=?,employmentDate=?,gender=?,contact_no=?,role=? WHERE empID='$id'";
@@ -175,13 +225,21 @@ require_once("../database/dbclass.php");
 	$stmt = $dbconn->prepare($sql);
 
 	$fname=$this->firstname;
+<<<<<<< HEAD
+	$lname=$this->lastname;
+=======
 	$laname=$this->lastname;
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 	$edate=$this->employmentdate;
 	$gen=$this->gender;
 	$contact=$this->contactnumber;
 	$rl=$this->role;
 
+<<<<<<< HEAD
+	$stmt->bind_param('i','s','s','s','s','i','s', $id,$fname,$lname,$edate,$gen,$contact,$rl);
+=======
 	$stmt->bind_param('i','s','s','s','s','i','s', $id,$fname,$lname,$edate,$gender,$contact,$rl);
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 	$stmt->execute(); 
     $stmt->close();
 }
--- webtechnology/final/database/databaseconnection.php
+++ webtechnology/final/database/databaseconnection.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php 
 /*
 *@author Tito Lulu Magero.
@@ -6,4 +7,14 @@
  $dname='n2018';
  $uname='root';
  $pwd='';
+=======
+<?php 
+/*
+*@author Tito Lulu Magero.
+*/
+ $hostname= 'localhost';
+ $dname='n2018';
+ $uname='root';
+ $pwd='';
+>>>>>>> 0852760d4beb3080a9f3e5457c744d64b9939870
 ?>
