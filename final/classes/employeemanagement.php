3f9705a58170149a9e5550e62a97b2b55062d059
veronicah.nanjala <veronicananjala1@gmail.com>
2017-04-17 19:56:22 +0000

    Starting testing again

---
+++ final/classes/employeemanagement.php
@@ -0,0 +1,187 @@
+<?php 
+/*
+*  This class helps the admin to add,delete and update emplyees information 
+*/
+
+/**
+* @author Veronicah Nanjala
+*/
+class EmplyoyeeManagement
+{
+	private $firstname;
+	private $lastname;
+	private $employeeid;
+	private $employmentdate;
+	private $gender;
+	private $contactnumber;
+	private $role;
+
+
+	function __construct()
+	{
+		# code...
+	}
+
+/*
+*Set the id of the employee 
+*/
+    /**
+     * @param $id
+     */
+public function setId($id){
+$this->employeeid=$id;
+}
+
+/*
+* return the id of the emplyee 
+* @return return employeeId
+*/
+public function getId(){
+return $this->employeeid;
+}
+
+/*
+* Set first name of the employee
+*/
+public function setFname($fname){
+$this->firstname=$fname;
+}
+
+/*
+* return first name of the employee
+*/
+public function getFname(){
+return $this->firstname;
+}
+
+/*
+*Set employee's last name 
+*/
+public function setLname($lname){
+$this->lastname=$lname;
+}
+
+/*
+* Return employee's last name
+*/
+public function getLname(){
+return $this->lastname;
+}
+
+/*
+* Set employee's gender
+*/
+public function setGender($gen){
+$this->gender=$gen;
+}
+
+/*
+* Return employee's gender
+*/
+public function getGender(){
+return $this->gender;
+}
+
+/*
+*           
+*/
+public function setEmploymentDate($date){
+$this->employmentdate=$date;
+}
+
+/*
+*
+*/
+public function getEmploymentDate(){
+return $this->employmentdate;
+}
+
+/*
+*
+*/
+public function setRole($rl){
+$this->role=$rl;
+}
+
+/*
+*
+*/
+public function getRole(){
+return $this->role;
+}
+
+/*
+*
+*/
+public function setContactNumber($contact){
+$this->contactnumber=$contact;
+}
+
+/*
+*
+*/
+public function getContactNumber(){
+return $this->contactnumber;
+}
+
+    /**
+     *Adding employee to the database
+     */
+    public function addEmployee(){
+    require_once("../database/dbclass.php");
+	$sql="INSERT INTO Employee empID=?,firts_name=?,last_name=?,employmentDate=?,gender=?,contact_no=?,role=?";
+
+	$stmt = $dbconn->prepare($sql);
+
+	$id=$this->employeeid;
+	$fname=$this->firstname;
+	$lname=$this->lastname;
+	$edate=$this->employmentdate;
+	$gen=$this->gender;
+	$contact=$this->contactnumber;
+	$rl=$this->role;
+
+	$stmt->bind_param('i','s','s','s','s','i','s', $id,$fname,$lname,$edate,$gen,$contact,$rl);
+	$stmt->execute(); 
+    $stmt->close(); 
+}
+
+/*
+*Deleting empployee from the database
+*/
+public function deleteEmployee(){
+require_once("../database/dbclass.php");
+	$id=$this->employeeid;
+	$sql="DELETE FROM Employee WHERE WHERE empID='$id'";
+
+	$stmt = $dbconn->prepare($sql);
+
+	$stmt->bind_param('i', $id);
+	$stmt->execute(); 
+    $stmt->close();
+}
+
+/*
+*Updating an emplyee in the database
+*/
+public function updateEmployee(){
+require_once("../database/dbclass.php");
+
+	$id=$this->employeeid;
+	$sql="UPDATE Employee SET firts_name=?,last_name=?,employmentDate=?,gender=?,contact_no=?,role=? WHERE empID='$id'";
+
+	$stmt = $dbconn->prepare($sql);
+
+	$fname=$this->firstname;
+	$lname=$this->lastname;
+	$edate=$this->employmentdate;
+	$gen=$this->gender;
+	$contact=$this->contactnumber;
+	$rl=$this->role;
+
+	$stmt->bind_param('i','s','s','s','s','i','s', $id,$fname,$lname,$edate,$gen,$contact,$rl);
+	$stmt->execute(); 
+    $stmt->close();
+}
+}
+?>
