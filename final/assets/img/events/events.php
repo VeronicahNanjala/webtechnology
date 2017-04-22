6fe34ca88351758c01c365c02d083e2a35c42ec2
TitoLulu <titolulu6@gmail.com>
2017-04-22 12:26:06 +0000

    modification to event page

--- final/assets/img/events/events.php
+++ final/assets/img/events/events.php
@@ -1,3 +1,5 @@
+<!--@Author: Tito Lulu Magero
+-->
 <!DOCTYPE html>
 <html>
 	<head>
@@ -15,13 +17,11 @@
 						<a href="audience.jpeg" data-lightbox= "This Month">
 							<img src="audience.jpg" class= "img-rounded" width="440px" height="300px">
 						</a>
-						<div class="panel panel-default">
-							<div class="panel-body" width="480px">>
+						<div class="panel">
 								<div class="caption">
 									<h3 >Lagos Photo</h3>
 									<p>Challenging  World's Afro-Pessimism</p>
 								</div>
-							</div>
 						</div>
 				</div>
 				<div class="col-lg-4 ">
@@ -29,27 +29,24 @@
 						<a href="album_launch.jpeg" data-lightbox= "This Month">
 							<img src="album_launch.jpg" class="img-rounded" width="440px" height="300px">
 						</a>
-						<div class="panel panel-default">
-							<div class="panel-body" width="480px">
+						<div class="panel">
 								<div class="caption">
 									<h3>Blanket & Wine Festival</h3>
 									<p>Explore African Music Through the Eyes of Nairobi</p>
 								</div>
-							</div>
 						</div>
+						
 				</div>
 				<div class="col-lg-4 " >
 						<!-- inserting an upcoming event images in the first column of the grid-->
 						<a href="dancehall.jpeg" data-lightbox= "This Month">
 							<img src="dancehall.jpg " class="img-rounded" width="440px" height="300px ">
 						</a>
-						<div class="panel panel-default">
-							<div class="panel-body" width="480px">
+						<div class="panel" >
 								<div class="caption">
 									<h3>Ultra Music Festival Johburg</h3>
 									<p>Live the experience of African House and Trap Music</p>
 								</div>
-							</div>
 						</div>
 				</div>
 			</div>
